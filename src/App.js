7875993876fe6269447d8b878c5ec5a199751ebd
gcerv93 <gcerv93@protonmail.com>
2022-04-29 08:45:18 -0500

    Fix Footer and Content layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Content />
+      <Footer />
     </div>
   );
 };
