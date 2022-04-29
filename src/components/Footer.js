831c673ca12bd3fa2aaf8a89753a6f19eca5f845
gcerv93 <gcerv93@protonmail.com>
2022-04-29 08:41:13 -0500

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Created by{" "}
+        <a href="https://github.com/gcerv93" target="_blank" rel="noreferrer">
+          gcerv93
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
