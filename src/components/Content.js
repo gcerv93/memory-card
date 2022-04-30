f74fd486385bb4f75465973e194ed5de73b99856
gcerv93 <gcerv93@protonmail.com>
2022-04-30 08:56:36 -0500

    Add Scores component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,11 @@
+import Scores from "./Scores";
+
 const Content = () => {
-  return <div className="content"></div>;
+  return (
+    <div className="content-box">
+      <Scores />
+    </div>
+  );
 };
 
 export default Content;
---
+++ src/components/Scores.js
@@ -0,0 +1,14 @@
+const Scores = (props) => {
+  return (
+    <div className="scores">
+      <div className="currScore">
+        <p>Current Score: 0</p>
+      </div>
+      <div className="bestScore">
+        <p>Best Score: 0</p>
+      </div>
+    </div>
+  );
+};
+
+export default Scores;
