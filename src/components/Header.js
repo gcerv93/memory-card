7a7611911a5042eb716d92097e4f4ef7a66764bd
gcerv93 <gcerv93@protonmail.com>
2022-05-03 17:38:46 -0500

    Add header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import "../styles/main.css";
+import HeaderImg from "../images/pokemon_memory.png";
 
 const Header = () => {
-  return <header>Pokemon Memory</header>;
+  return (
+    <header>
+      <img src={HeaderImg} alt="" height="100px"></img>
+    </header>
+  );
 };
 
 export default Header;
