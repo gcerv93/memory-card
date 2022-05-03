8d1f9027679564375dccd81929b6a2d52164a66d
gcerv93 <gcerv93@protonmail.com>
2022-05-03 17:21:05 -0500

    Move state handling up a level to Content component
    Previously I had state handling in the Cards component, but this was the
    wrong choice. In order to track the scores throughout the app, state
    should be handled in the Content component and then passed onto to both
    the Scores component and the Cards component.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,66 +1,16 @@
-import { useState, useEffect } from "react";
-import { shuffleArray } from "../utils";
 import Card from "./Card";
 
-const Cards = () => {
-  const CARD_AMOUNT = 12;
-  const [displays, setDisplays] = useState([]);
-  const [clicked, setClicked] = useState([]);
-  const [currentScore, setCurrentScore] = useState(0);
-  const [bestScore, setBestScore] = useState(0);
-
-  async function fetchData(num) {
-    const displayData = [];
-
-    for (let i = 1; i <= num; i++) {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-      const data = await response.json();
-
-      const dataID = data.id;
-      const dataName = data.name;
-      const dataImage = data.sprites.front_default;
-
-      displayData.push({ dataID, dataName, dataImage });
-    }
-
-    return displayData;
-  }
-
-  function handleClicks(id) {
-    if (clicked.indexOf(id) === -1) {
-      setClicked(clicked.concat(id));
-      setCurrentScore((prevScore) => prevScore + 1);
-      if (currentScore >= bestScore) {
-        setBestScore((prevScore) => prevScore + 1);
-      }
-    } else {
-      setClicked([]);
-      setCurrentScore(0);
-      setBestScore((prevScore) => prevScore);
-    }
-
-    setDisplays(shuffleArray(displays));
-  }
-
-  useEffect(() => {
-    console.log("hello");
-    const loadData = async () => {
-      setDisplays(shuffleArray(await fetchData(CARD_AMOUNT)));
-    };
-
-    loadData();
-  }, []);
-
+const Cards = (props) => {
   return (
     <div className="cards">
-      {displays.length > 0
-        ? displays.map((info, idx) => (
+      {props.cards.length > 0
+        ? props.cards.map((info, idx) => (
             <Card
               key={idx}
               id={info.dataID}
               name={info.dataName}
               image={info.dataImage}
-              clickHandler={handleClicks}
+              clickHandler={props.clickHandler}
             />
           ))
         : null}
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,61 @@
 import Scores from "./Scores";
 import Cards from "./Cards";
+import { useState, useEffect } from "react";
+import { shuffleArray } from "../utils";
 
 const Content = () => {
+  const CARD_AMOUNT = 12;
+  const [cards, setCards] = useState([]);
+  const [clicked, setClicked] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  async function fetchData(num) {
+    const cardsData = [];
+
+    for (let i = 1; i <= num; i++) {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+      const data = await response.json();
+
+      const dataID = data.id;
+      const dataName = data.name;
+      const dataImage = data.sprites.front_default;
+
+      cardsData.push({ dataID, dataName, dataImage });
+    }
+
+    return cardsData;
+  }
+
+  function handleClicks(id) {
+    if (clicked.indexOf(id) === -1) {
+      setClicked(clicked.concat(id));
+      setCurrentScore((prevScore) => prevScore + 1);
+      if (currentScore >= bestScore) {
+        setBestScore((prevScore) => prevScore + 1);
+      }
+    } else {
+      setClicked([]);
+      setCurrentScore(0);
+      setBestScore((prevScore) => prevScore);
+    }
+
+    setCards(shuffleArray(cards));
+  }
+
+  useEffect(() => {
+    console.log("hello");
+    const loadData = async () => {
+      setCards(shuffleArray(await fetchData(CARD_AMOUNT)));
+    };
+
+    loadData();
+  }, []);
+
   return (
     <div className="content-box">
       <Scores />
-      <Cards />
+      <Cards cards={cards} clickHandler={handleClicks} />
     </div>
   );
 };
